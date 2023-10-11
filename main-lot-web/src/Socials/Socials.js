d9a044fd9386a5d8e56678c7ffafd9ff53a1e7a3
Gikai Andrews <gnaboys@gmail.com>
2023-10-11 00:52:27 -0400

    adjusted css and started socials

--- main-lot-web/src/Socials/Socials.js
+++ main-lot-web/src/Socials/Socials.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./Socials.css";
+// import titwitch from '../images/ti-twitch.mp4'
 
 function Socials() {
   return (
     <div className="content-section">
-      <a href="https://www.twitch.tv/legendof_ti">
-        <button className="social-subsection-twitch-button">Twitch</button>
-      </a>
+      <a href="https://www.twitch.tv/legendof_ti" className="twitch-link">
+        <button className="social-subsection-twitch-button">Twitch</button></a>
+        {/* <video autoPlay loop muted className='ti-twitch'>
+          <source src={titwitch} type='video/mp4' />
+        </video> */}
+      
     </div>
   );
 }
