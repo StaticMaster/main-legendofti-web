9a1f22a0a8db5517bab92db197db850a5da47e5e
Gikai Andrews <gnaboys@gmail.com>
2023-10-16 12:00:06 -0400

    added twitch vid sample and adjusted sizing

--- main-lot-web/src/Socials/Socials.js
+++ main-lot-web/src/Socials/Socials.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./Socials.css";
-// import titwitch from '../images/ti-twitch.mp4'
+import titwitch from '../images/ti-twitch-updated.mp4'
 
 function Socials() {
   return (
     <div className="content-section">
       <a href="https://www.twitch.tv/legendof_ti" className="twitch-link">
         <button className="social-subsection-twitch-button">Twitch</button></a>
-        {/* <video autoPlay loop muted className='ti-twitch'>
+        <video autoPlay loop muted className='ti-twitch'>
           <source src={titwitch} type='video/mp4' />
-        </video> */}
+        </video>
       
     </div>
   );
